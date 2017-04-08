0ad46997ce8fb35fc789a5b07276eb058c8bc956
Maciej Michalski <michalski.maciej94@gmail.com>
2017-04-08 16:16:11 +0200

    change

--- js/6-animacje.js
+++ js/6-animacje.js
@@ -2,10 +2,19 @@ $(function(){
     'use strict';
     $('h1').hide(5000).show(3000);
     $('h2').fadeOut(5000).fadeIn(3000);
+    
+    var rand = Math.floor((Math.random()*100));
+    console.log(rand);
    
-    slide();
+   /* slide();
     
     function slide(){
-         $('h3').slideUp(5000).slideDown(3000, slide); // jak się skończy funkcja ,to zacznij funkcję slide(funkcja wywołuje funkcję)
+         $('h3').slideUp(5000).slideDown(3000, slide); // jak się skończy funkcja ,to zacznij funkcję slide(funkcja wywołuje funkcję - callback)
+    }*/
+    
+    $('h3').animate({'font-size' : '1em', 'margin-left' : rand + '100px'}, 2000, parBack);
+    
+    function parBack(){
+        $('p').css('background', 'red');
     }
 });
